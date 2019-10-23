5a08536cde2e1c018679d8ca324499a63370c53e
Hassib Ashouri <hassib291@hotmail.com>
2019-10-22 21:46:52 -0700

    added aws config for cognito

---
+++ aws-exports.js
@@ -0,0 +1,8 @@
+export default {
+	cognito: {
+		REGION: 'us-east-2',
+		USER_POOL_ID: 'us-east-2_c7Q0yBBzL',
+		APP_CLIENT_ID: '4faikckctrbp8ubobc7un3p4l9',
+		IDENTITY_POOL_ID: 'us-east-2:2874d2df-b21e-4ecb-b289-d704f72123cc'
+	}
+};
