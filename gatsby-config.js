72b493c2684dff1e169d7c096ef0bbb0ca70dc60
Hassib Ashouri <hassib291@hotmail.com>
2019-11-12 14:55:35 -0800

    packages and files not needed

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-material-ui`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
